f3a63a2be83a775e0b36e787e23620b509303646
Marwa Ters <marwaters94@gmail.com>
2022-07-03 20:16:35 +0200

    creation des bibliotheques de class

---
+++ Holiday.Rental.DAL/Repositories/ServiceBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Holiday.Rental.DAL.Repositories
+{
+    public abstract class ServiceBase
+    {
+        protected string _connString = @"Data Source=LAPTOP-2B1BFEK4;Initial Catalog=HoliDayRental.DB;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
